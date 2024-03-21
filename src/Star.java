93649603ef2c652300012c4b5f524cec5d21adc7
SongChanWoo <mu9800mu12@naver.com>
2024-03-21 23:40:51 +0900

    별짓기

--- src/Star.java
+++ src/Star.java
@@ -5,21 +5,13 @@ public class Star {
 
         int a = 0;
 
-        int b =0;
-        for(a=0 ; a<7; a++){
+        int b = 0;
+        for (a = 0; a < 7; a++) {
 
-            System.out.print("*");
-        if(a>1) {
-            for (b = 0; b < 1; b++) {
-                System.out.println();
-            }
-        }else {
-                System.out.printf("2");
-            }
 
 
         }
-
-
     }
+
+
 }
