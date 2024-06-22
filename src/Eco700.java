6b19d7fe7bc351a04a25096f6de138b799a34b80
SongChanWoo <mu9800mu12@naver.com>
2024-06-22 23:30:25 +0900

    단어 추출

--- src/Eco700.java
+++ src/Eco700.java
@@ -24,9 +24,6 @@ public class Eco700 {
 
 
 
-
-
-
     // 용어와 설명 추출하는 메서드
     public static Map<String, String> extractTermsAndDefinitions(String text) {
         Map<String, String> termsAndDefinitions = new HashMap<>();
